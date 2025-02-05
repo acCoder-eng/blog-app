420ee325a0c6d6bceee0ba6c2f8268c12e597e61
acCoder-eng <citirahmett0@gmail.com>
2025-02-05 14:43:55 +0300

    first0502

---
+++ Comment-Microservice/src/test/java/com/example/Comment/Microservice/CommentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Comment.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
